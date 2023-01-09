5b08b5aca608353b15bcf12a50e2bac1b8bf5412
Leon Teale <leonteale89@gmail.com>
2023-01-09 16:46:53 -0500

    .

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ if (trim($output) != "") {
     $domain = $matches[1];
     echo "Search currently in progress for $domain";
 } else {
-    echo "Program free for wildcar search";
+    echo "Program free for wildcard search. Search <a href="">here</a>";
 }
 
 ?>
