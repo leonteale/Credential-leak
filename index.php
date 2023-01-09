bbf17c13253d79a6cb487a2f8bc4e18b8ec5f17f
Leon Teale <leonteale89@gmail.com>
2023-01-09 10:53:08 -0500

    .

--- index.php
+++ index.php
@@ -91,18 +91,21 @@
     <th>Description</th>
   </tr>
   
-  <?php
+<?php
 
 $command = "ps aux | grep '/home/leon/.local/bin/h8mail' | grep -v grep";
 $output = shell_exec($command);
 
 if (trim($output) != "") {
-    echo "Search currently running";
+    preg_match('/-t ([^\s]+)/', $output, $matches);
+    $domain = $matches[1];
+    echo "Search currently in progress for $domain";
 } else {
     echo "Program free to search";
 }
 
 ?>
+
   <?php
     $dir = '../wildcard';
     $files = scandir($dir);
