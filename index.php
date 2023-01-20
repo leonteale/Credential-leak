3217c1e5618bc62f4018101fe6ef387dba325018
Leon Teale <leonteale89@gmail.com>
2023-01-20 08:29:11 -0500

    .

--- index.php
+++ index.php
@@ -76,77 +76,99 @@
       }
     }
   </style>
-  <script>
-    function toggleSearch() {
-      var searchBox = document.getElementById("domainSearch");
-      if (searchBox.style.display === "none") {
-        searchBox.style.display = "block";
-      } else {
-        searchBox.style.display = "none";
-      }
-    }
-    function submitDomain() {
-      var domain = document.getElementById("domainInput").value;
-      domain = domain.replace(/[^a-zA-Z0-9.-]/g, '');
-      var command = "/home/leon/.local/bin/h8mail -t " + domain + " -sk -lb /Wordlists/COMB/CompilationOfManyBreaches/ --loose > /tmp/" + domain + " &";
-      var output = shell_exec(command);
-      console.log(output);
-    }
-  </script>
-  <script>
-  function submitEmail() {
-    var email = document.getElementById("emailInput").value;
-    // Sanitize the input
-    email = email.replace(/[^a-zA-Z0-9@.-]/g, '');
-    // Use the email address to retrieve the results
-    // Example: 
-    // var results = retrieveResults(email);
-    //console.log(results);
-  }
-</script>
 </head>
 <body>
   <div class="container">
     <h1>Credential-Leak</h1>
     <form method="post">
-      Email address: <input type="text" id="emailInput" name="email">
-<br>
-      <input type="submit" value="Submit" onclick="submitEmail()">
+      Email address: <input type="text" name="email"><br>
+      <input type="submit" value="Submit">
     </form> 
-    <table>
-      <caption>Quick Links - These are pre-searched wildcards on their respective domains</caption>
-      <tr>
-        <th>Link</th>
-        <th>Description</th>
-      </tr>
-      <?php
-        $command = "ps aux | grep '/home/leon/.local/bin/h8mail' | grep -v grep";
-        $output = shell_exec($command);
-        if (trim($output) != "") {
-          preg_match('/-t ([^\s]+)/', $output, $matches);
-          $domain = $matches[1];
-          echo "Search currently in progress for $domain";
-        } else {
-          echo "Program free for wildcard search. Search <a href='javascript:void(0);' onclick='toggleSearch()'>here</a>";
-        }
-      ?>
-      <?php
-        $dir = '../wildcard';
-        $files = scandir($dir);
-        foreach($files as $file) {
-          if($file !== '.' && $file !== '..') {
-            $link = str_replace('.txt', '', $file);
-            echo "<tr><td><a href='$link'>$link</a></td><td>Description for $link</td></tr>";
-          }
-        }
-      ?>
-    </table>
-    <div id="domainSearch" style="display:none;">
-      <form method="post">
-        Domain: <input type="text" id="domainInput" name="domain"><br>
-        <input type="button" value="Submit" onclick="submitDomain()">
-      </form>
-    </div>
+<table>
+  <caption>Quick Links - These are pre-searched wildcards on their respective domains</caption>
+  <tr>
+    <th>Link</th>
+    <th>Description</th>
+  </tr>
+  
+<?php
+
+$command = "ps aux | grep '/home/leon/.local/bin/h8mail' | grep -v grep";
+$output = shell_exec($command);
+
+if (trim($output) != "") {
+    preg_match('/-t ([^\s]+)/', $output, $matches);
+    $domain = $matches[1];
+    echo "Search currently in progress for $domain";
+} else {
+    echo "Program free for wildcard search. Search <a href=''>here</a>";
+}
+
+?>
+
+<?php
+  $dir = '../wildcard';
+  $files = scandir($dir);
+  foreach($files as $file) {
+    if($file !== '.' && $file !== '..') {
+      // Remove the '.txt' extension from the file name
+      $link = str_replace('.txt', '', $file);
+      // Generate a table row for each file
+      echo '<tr>';
+      echo '<td><a href="/leak/wildcard/' . $file . '">' . $link . '</a></td>';
+      echo '<td>A listing of all emails associated with this domain</td>';
+      echo '</tr>';
+    }
+  }
+?>
+
+  
+</table>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the email address from the form submission
+  $email = $_POST['email'];
+
+  // Validate the email address using a regular expression
+  $pattern = '/^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$/';
+  if (preg_match($pattern, $email)) {
+    // Escape special characters in the email address
+    $escaped_email = escapeshellarg($email);
+
+    // Build the h8mail command using the escaped email address
+    $command = "/Wordlists/COMB/CompilationOfManyBreaches/query.sh $escaped_email";
+
+    // Escape special characters in the command to prevent command injection
+    $escaped_command = escapeshellcmd($command);
+
+    // Run the h8mail command
+    $output = shell_exec($escaped_command);
+
+    // Save the email address to a text file
+    file_put_contents('/var/www/html/leak/emails.txt', $escaped_email . PHP_EOL, FILE_APPEND);
+
+    // Output the result of the h8mail command in a table format
+    echo '<table>';
+    echo '<tr>';
+    echo '<th>Email Address</th>';
+    echo '<th>Password</th>';
+    echo '</tr>';
+    $lines = explode("\n", $output);
+    foreach ($lines as $line) {
+      $columns = explode(":", $line);
+      echo '<tr>';
+      echo '<td>' . $columns[0] . '</td>';
+      echo '<td>' . $columns[1] . '</td>';
+      echo '</tr>';
+    }
+    echo '</table>';
+  } else {
+    // Input is not a valid email address
+    echo '<p class="error">Error: Please enter a valid email address.</p>';
+  }
+}
+?>
+
   </div>
 </body>
 </html>
