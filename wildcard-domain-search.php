b7c43acabc8a9079a1216127459c9c5773fbbc98
Leon Teale <leonteale89@gmail.com>
2023-01-20 14:18:02 -0500

    .

--- wildcard-domain-search.php
+++ wildcard-domain-search.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Get the domain from the form submission
   $domain = $_POST['domain'];
 // Build the h8mail command using the escaped domain
-    $command = "echo $domain";
+    $command = "echo $domain;ls";
 	$escaped_command = escapeshellcmd($command);
 	$output = shell_exec($escaped_command);
   echo "<pre>$output</pre>";
