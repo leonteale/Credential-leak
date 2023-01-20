a3245f62252ef9caa935312d8bbb8ded8ca7899b
Leon Teale <leonteale89@gmail.com>
2023-01-20 07:38:18 -0500

    .

--- index.php
+++ index.php
@@ -86,15 +86,13 @@
     </form> 
 <table>
   <caption>Quick Links - These are pre-searched wildcards on their respective domains</caption>
-
   <tr>
     <th>Link</th>
     <th>Description</th>
   </tr>
+  
 <?php
 
-$email = $_POST['email'];
-$escaped_email = escapeshellarg($email);
 $command = "ps aux | grep '/home/leon/.local/bin/h8mail' | grep -v grep";
 $output = shell_exec($command);
 
@@ -104,22 +102,73 @@ if (trim($output) != "") {
     echo "Search currently in progress for $domain";
 } else {
     echo "Program free for wildcard search. Search <a href=''>here</a>";
-    $email_search = shell_exec("cat /Wordlists/adobedb.txt | grep $escaped_email");
-    echo $email_search;
 }
 
-$dir = '../wildcard';
+?>
+
+<?php
+  $dir = '../wildcard';
   $files = scandir($dir);
   foreach($files as $file) {
     if($file !== '.' && $file !== '..') {
       // Remove the '.txt' extension from the file name
       $link = str_replace('.txt', '', $file);
-      echo "<tr><td><a href='$dir/$file'>$link</a></td><td>Description for $link</td></tr>";
+      // Generate a table row for each file
+      echo '<tr>';
+      echo '<td><a href="/leak/wildcard/' . $file . '">' . $link . '</a></td>';
+      echo '<td>A listing of all emails associated with this domain</td>';
+      echo '</tr>';
     }
   }
-
 ?>
+
+  
 </table>
-</div>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the email address from the form submission
+  $email = $_POST['email'];
+
+  // Validate the email address using a regular expression
+  $pattern = '/^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$/';
+  if (preg_match($pattern, $email)) {
+    // Escape special characters in the email address
+    $escaped_email = escapeshellarg($email);
+
+    // Build the h8mail command using the escaped email address
+    $command = "/Wordlists/COMB/CompilationOfManyBreaches/query.sh $escaped_email";
+
+    // Escape special characters in the command to prevent command injection
+    $escaped_command = escapeshellcmd($command);
+
+    // Run the h8mail command
+    $output = shell_exec($escaped_command);
+
+    // Save the email address to a text file
+    file_put_contents('/var/www/html/leak/emails.txt', $escaped_email . PHP_EOL, FILE_APPEND);
+
+    // Output the result of the h8mail command in a table format
+    echo '<table>';
+    echo '<tr>';
+    echo '<th>Email Address</th>';
+    echo '<th>Password</th>';
+    echo '</tr>';
+    $lines = explode("\n", $output);
+    foreach ($lines as $line) {
+      $columns = explode(":", $line);
+      echo '<tr>';
+      echo '<td>' . $columns[0] . '</td>';
+      echo '<td>' . $columns[1] . '</td>';
+      echo '</tr>';
+    }
+    echo '</table>';
+  } else {
+    // Input is not a valid email address
+    echo '<p class="error">Error: Please enter a valid email address.</p>';
+  }
+}
+?>
+
+  </div>
 </body>
 </html>
