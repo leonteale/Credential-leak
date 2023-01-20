69229cd249507099ba635985d549716246bacabb
Leon Teale <leonteale89@gmail.com>
2023-01-20 13:16:54 -0500

    .

--- wildcard-domain-search.php
+++ wildcard-domain-search.php
@@ -1,29 +1,7 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Get the email address from the form submission
+  <?php
+if(isset($_POST['domain'])) {
   $domain = $_POST['domain'];
-
-  // Validate the email address using a regular expression
-  $pattern = '/[^a-zA-Z0-9.-]/';
-  if (preg_match($pattern, $domain)) {
-    // Escape special characters in the domain
-    $escaped_domain = escapeshellarg($domain);
-
-    // Build the h8mail command using the escaped email address
-    $command = "ls";
-
-    // Escape special characters in the command to prevent command injection
-    $escaped_command = escapeshellcmd($command);
-
-    // Run the h8mail command
-    $output = shell_exec($escaped_command);
-
-    // Output the result of the h8mail command in a table format
-    echo 'crunning search on ' + $escaped_domain;
-	echo $output
-  } else {
-    // Input is not a valid email address
-    echo '<p class="error">Error: Please enter a valid domain.</p>';
-  }
+  $output = exec("dig $domain");
+  echo "<pre>$output</pre>";
 }
 ?>
